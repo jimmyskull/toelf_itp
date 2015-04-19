d87c92d95463d5046f705d8fdbbc1e7a0585849d
Paulo Roberto Urio <urio@usp.br>
2015-04-19 10:34:30 -0300

    Optimizing scores as parameters instead of questions
    Signed-off-by: Paulo Roberto Urio <urio@usp.br>

--- itp.py
+++ itp.py
@@ -19,26 +19,27 @@ N3 = lambda x: floor(x / 50 * (67 - 31) + 31)
 # Separated score → total score
 N = lambda a, b, c: (a + b + c - 31 * 3) * (677-310) / (68+68+67-31*3) + 310
 NQ = lambda a, b, c: N(N1(a), N2(b), N3(c))
+QN = lambda a, b, c: np.array([Q1(a), Q2(b), Q3(c)])
 
-TARGET = 630 # The minimum for proficiency is 627
+TARGET = 628 # The minimum for proficiency is 627
 def f(x):
-    return abs(TARGET - NQ(*x))
+    return abs(TARGET - N(*x))
 
 def TOEFL_iTP(n1, n2, n3):
     print('Section 1: {} points, {}/50 ({} missed)'.format(n1, Q1(n1), 50-Q1(n1)))
     print('Section 2: {} points, {}/40 ({} missed)'.format(n2, Q2(n2), 40-Q2(n2)))
     print('Section 3: {} points, {}/50 ({} missed)'.format(n3, Q3(n3), 50-Q3(n3)))
-    print('Total: {}/677'.format(floor(N(n1, n2, n3))))
-    parameters = np.array([Q1(n1), Q2(n2), Q3(n3)])
-    bounds = ((0, 50), (0, 40), (0, 50))
-    r = optimize.minimize(f, parameters, method = 'SLSQP', bounds=bounds, options={
-            'disp':True, 'eps': 1.0})
-    print('Your result:', parameters)
-    print('Optimized:', r.x)
+    print('Total: {}/677\n'.format(floor(N(n1, n2, n3))))
+    parameters = np.array([n1, n2, n3])
+    bounds = ((31, 68), (31, 68), (31, 67))
+    r = optimize.minimize(f, parameters, method = 'SLSQP', bounds=bounds,\
+            options={'disp': True, 'eps': 1.0})
+    print('\nYour result:', QN(*parameters))
+    print('Optimized  :', QN(*r.x))
     print('How many more questions you would have to get correct:\n ' + \
-        'Section 1: {}\n Section 2: {}\n Section 3: {}\n'.format(*(r.x - parameters)))
-    print('And then you’d get a score of {}'.format(NQ(*r.x)))
+            'Section 1: {}\n Section 2: {}\n Section 3: {}\n'.format(\
+                *(QN(*r.x) - QN(*parameters))))
+    print('And then you’d get a score of {}'.format(NQ(*QN(*r.x))))
 
 TOEFL_iTP(n1=64, n2=63, n3=57)
-
 
